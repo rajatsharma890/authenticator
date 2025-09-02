d4439e8943e81c843276e0c3d742acf704be5dbd
Rajat Sharma <rajatsharma302001@gmail.com>
2025-09-02 18:40:14 +0530

    modified

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -6,9 +6,9 @@ export const dynamic = "force-dynamic";
 export default async function ProfilePage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
-  const { id } = params;
+  const { id } = await params;
   const user = await User.findById(id);
 
   if (!user) {
